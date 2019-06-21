779a8a425d19a83b4b59dcba01bdf9077a13b585
Matthew Tan <matthewvtan@gmail.com>
2019-06-21 00:30:04 -0500

    alternate method for server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require ('mongoose');
 const eventRoutes = express.Router();
-const PORT = process.env.PORT || 4000;
 const path = require("path");
 require("dotenv").config();
 
@@ -81,9 +80,15 @@ eventRoutes.route('/update/:id').post(function(req, res) {
 
 app.use('/events', eventRoutes);
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, function() {
   console.log("Server is running on Port: " + PORT);
