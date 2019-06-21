4a3ad6e47c6adec4d59d87d90d8be98b21ebfe38
Matthew Tan <matthewvtan@gmail.com>
2019-06-20 23:39:41 -0500

    second

---
+++ event.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Event = new Schema({
+    title: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    job_address: {
+        type: String
+    },
+    start: {
+        type: String
+    },
+    end: {
+        type: String
+    },
+    work_requested: {
+        type: String
+    },
+    date_repaired: {
+        type: String
+    },
+    performed_by: {
+        type: String
+    },
+    repairs_performed: {
+        type: String
+    },
+    labor: {
+        type: String
+    },
+    hours: {
+        type: String
+    },
+    materials: {
+        type: String
+    },
+    room: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+});
+
+module.exports = mongoose.model('Event', Event);
---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require ('mongoose');
+const eventRoutes = express.Router();
+const PORT = process.env.PORT || 4000;
+const path = require("path");
+require("dotenv").config();
+
+let Event = require('./event.model');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, '/client/build')));
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/events', { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+eventRoutes.route('/').get(function(req, res) {
+  Event.find(function(err, events) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(events);
+    }
+  });
+});
+
+eventRoutes.route('/:id').get(function(req, res) {
+  let id = req.params.id;
+  Event.findById(id, function(err, event) {
+    res.json(event);
+  });
+});
+
+eventRoutes.route('/add').post(function(req, res) {
+  let event = new Event(req.body);
+  event.save()
+    .then(event => {
+      res.status(200).json({'event': 'event added successfully'});
+    })
+    .catch(err => {
+      res.status(400).send('adding new event failed');
+    });
+});
+
+eventRoutes.route('/update/:id').post(function(req, res) {
+  Event.findById(req.params.id, function(err, event) {
+    if (!event)
+      res.status(404).send('data is not found');
+    else
+      event.title = req.body.title;
+      event.phone = req.body.phone;
+      event.address = req.body.address;
+      event.job_address = req.body.job_address;
+      event.start = req.body.start;
+      event.end = req.body.end;
+      event.work_requested = req.body.work_requested;
+      event.date_repaired = req.body.date_repaired;
+      event.performed_by = req.body.performed_by;
+      event.repairs_performed = req.body.repairs_performed;
+      event.labor = req.body.labor;
+      event.hours = req.body.hours;
+      event.materials = req.body.materials;
+      event.room = req.body.room;
+      event.completed = req.body.completed;
+      
+      event.save().then(event => {
+        res.json('Event updated');
+      })
+      .catch(err => {
+        res.status(400).send("Update not possible");
+      });
+  });
+});
+
+app.use('/events', eventRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
+app.listen(PORT, function() {
+  console.log("Server is running on Port: " + PORT);
+});
