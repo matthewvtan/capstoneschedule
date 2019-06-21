5d8c238ed19417d9c20a8731cb897d73dd533e89
Matthew Tan <matthewvtan@gmail.com>
2019-06-21 01:31:58 -0500

    Changed port alternate in server.js to match calls from axios in client

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server is running on Port: ${port}`));
 
