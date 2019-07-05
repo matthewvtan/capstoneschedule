7a0c8456473ff8f45323956023a945ceda050cf4
Matthew Tan <matthewvtan@gmail.com>
2019-07-05 16:33:18 -0500

    Fixing app.js to include import to home component

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
-export default class EventsList extends Component {
+export default class Home extends Component {
     
     render() {
         return (
             <div>
                 <li>
-                    <Link to="/" className="nav-link">List</Link>
+                    <Link to="/list" className="nav-link">List</Link>
                 </li>
                 <li>
                     <Link to="/create" className="nav-link">Add</Link>
