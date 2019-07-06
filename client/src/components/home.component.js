e0b9140fcfb4b9db008ad280344b05e0dd4a9dd9
Matthew Tan <matthewvtan@gmail.com>
2019-07-06 00:07:13 -0500

    added button styling

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -7,18 +7,18 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <li>
+                <a href="#" class="btn-gradient blue block">
                     <Link to="/list" className="nav-link">List</Link>
-                </li>
-                <li>
-                    <Link to="/create" className="nav-link">Add</Link>
-                </li>
-                <li>
-                    <Link to="/calendar" className="nav-link">Calendar</Link> 
-                </li>
-                <li>
-                    <Link to="/export" className="nav-link">Export</Link> 
-                </li>
+                </a>
+                <a href="#" class="btn-gradient blue block">
+                    <Link to="/create" className="nav-link">New Order</Link>
+                </a>
+                <a href="#" class="btn-gradient blue block">
+                    <Link to="/calendar" className="nav-link">Calendar</Link>
+                </a>
+                <a href="#" class="btn-gradient blue block">
+                    <Link to="/export" className="nav-link">Export</Link>
+                </a>
             </div>
         )
     }
