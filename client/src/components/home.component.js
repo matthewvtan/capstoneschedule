162e13e71dfa54a3d356dd369a2d5ab72369e090
Matthew Tan <matthewvtan@gmail.com>
2019-07-06 00:15:51 -0500

    Added styling to buttons on Home component

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -10,12 +10,15 @@ export default class Home extends Component {
                 <a href="#" class="btn-gradient blue block">
                     <Link to="/list" className="nav-link">List</Link>
                 </a>
+                <br />
                 <a href="#" class="btn-gradient blue block">
                     <Link to="/create" className="nav-link">New Order</Link>
                 </a>
+                <br />
                 <a href="#" class="btn-gradient blue block">
                     <Link to="/calendar" className="nav-link">Calendar</Link>
                 </a>
+                <br />
                 <a href="#" class="btn-gradient blue block">
                     <Link to="/export" className="nav-link">Export</Link>
                 </a>
