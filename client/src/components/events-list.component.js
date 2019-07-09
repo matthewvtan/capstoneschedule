50d57a5af1a05fa1066fea6780de03f7d13e9b3f
Matthew Tan <matthewvtan@gmail.com>
2019-07-09 11:01:45 -0500

    removed new table styles

--- client/src/components/events-list.component.js
+++ client/src/components/events-list.component.js
@@ -2,43 +2,35 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import moment from 'moment';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import '../App.css';
-import 'typeface-roboto';
 
 const Event = props => (
-  <TableRow>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.title}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.phone}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.email_address}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}><a href={"https://www.google.com/maps/search/?api=1&query="+props.event.job_address}>{props.event.job_address}</a></TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{moment(props.event.start).format("MM/DD/YYYY hh:mm a")}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{moment(props.event.end).format("MM/DD/YYYY hh:mm a")}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.work_requested}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{moment(props.event.date_repaired).format("MM/DD/YYYY")}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.performed_by}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.repairs_performed}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.labor}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.hours}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.materials}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.room}</TableCell>
-    <TableCell>
+  <tr>
+    <td className={props.event.completed ? 'completed' : ''}>{props.event.title}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{props.event.phone}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{props.event.email_address}</td>
+    <td className={props.event.completed ? 'completed' : ''}><a href={"https://www.google.com/maps/search/?api=1&query="+props.event.job_address}>{props.event.job_address}</a></td>
+    <td className={props.event.completed ? 'completed' : ''}>{moment(props.event.start).format("MM/DD/YYYY hh:mm a")}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{moment(props.event.end).format("MM/DD/YYYY hh:mm a")}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{props.event.work_requested}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{moment(props.event.date_repaired).format("MM/DD/YYYY")}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{props.event.performed_by}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{props.event.repairs_performed}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{props.event.labor}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{props.event.hours}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{props.event.materials}</td>
+    <td className={props.event.completed ? 'completed' : ''}>{props.event.room}</td>
+    <td>
         <Link to={"/edit/"+props.event._id}>Edit</Link>
-    </TableCell>
-    <TableCell>
+    </td>
+    <td>
         <Link to={"/complete/"+props.event._id}>Complete</Link>
-    </TableCell>
-  </TableRow>
+    </td>
+  </tr>
 )
 
 export default class EventsList extends Component {
-
+  
     constructor(props) {
       super(props);
       this.state = {events: []};
@@ -77,104 +69,37 @@ export default class EventsList extends Component {
         return <Event event={currentEvent} key={i} />
       });
     }
-
+    
     render() {
         return (
-            <Paper>
+            <div>
                 <h3 style={{marginTop: 20}}>Work Orders</h3>
-                <Table style={{ marginTop: 20}}>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Client Name</TableCell>
-                      <TableCell>Phone</TableCell>
-                      <TableCell>Client Email Address</TableCell>
-                      <TableCell>Job Address</TableCell>
-                      <TableCell>Date / Start Time</TableCell>
-                      <TableCell>End Time</TableCell>
-                      <TableCell>Work Requested</TableCell>
-                      <TableCell>Date Repaired</TableCell>
-                      <TableCell>Performed By</TableCell>
-                      <TableCell>Repairs Performed</TableCell>
-                      <TableCell>Labor</TableCell>
-                      <TableCell>Hours</TableCell>
-                      <TableCell>Materials</TableCell>
-                      <TableCell>Room</TableCell>
-                      <TableCell>Edit</TableCell>
-                      <TableCell>Complete</TableCell>
-                    </TableRow>
-                  </TableHead>
-                    <TableBody>
+                <table className="table-responsive table-bordered" style={{ marginTop: 20}}>
+                  <thead>
+                    <tr>
+                      <th>Client Name</th>
+                      <th>Phone</th>
+                      <th>Client Email Address</th>
+                      <th>Job Address</th>
+                      <th>Date / Start Time</th>
+                      <th>End Time</th>
+                      <th>Work Requested</th>
+                      <th>Date Repaired</th>
+                      <th>Performed By</th>
+                      <th>Repairs Performed</th>
+                      <th>Labor</th>
+                      <th>Hours</th>
+                      <th>Materials</th>
+                      <th>Room</th>
+                      <th>Edit</th>
+                      <th>Complete</th>
+                    </tr>
+                  </thead>
+                    <tbody>
                       { this.eventList() }
-                    </TableBody>
-                </Table>
-            </Paper>
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Table from '@material-ui/core/Table';
-// import TableBody from '@material-ui/core/TableBody';
-// import TableCell from '@material-ui/core/TableCell';
-// import TableHead from '@material-ui/core/TableHead';
-// import TableRow from '@material-ui/core/TableRow';
-// import Paper from '@material-ui/core/Paper';
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     width: '100%',
-//     marginTop: theme.spacing(3),
-//     overflowX: 'auto',
-//   },
-//   table: {
-//     minWidth: 650,
-//   },
-// }));
-
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
-
-// export default function SimpleTable() {
-//   const classes = useStyles();
-
-//   return (
-//     <Paper className={classes.root}>
-//       <Table className={classes.table}>
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>Dessert (100g serving)</TableCell>
-//             <TableCell align="right">Calories</TableCell>
-//             <TableCell align="right">Fat&nbsp;(g)</TableCell>
-//             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-//             <TableCell align="right">Protein&nbsp;(g)</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {rows.map(row => (
-//             <TableRow key={row.name}>
-//               <TableCell component="th" scope="row">
-//                 {row.name}
-//               </TableCell>
-//               <TableCell align="right">{row.calories}</TableCell>
-//               <TableCell align="right">{row.fat}</TableCell>
-//               <TableCell align="right">{row.carbs}</TableCell>
-//               <TableCell align="right">{row.protein}</TableCell>
-//             </TableRow>
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </Paper>
-//   );
-// }
