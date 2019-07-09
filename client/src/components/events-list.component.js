63ee8dabc1968b2a0fa7608b9a78cd0426bd003e
Matthew Tan <matthewvtan@gmail.com>
2019-07-09 14:42:27 -0500

    aligning table content

--- client/src/components/events-list.component.js
+++ client/src/components/events-list.component.js
@@ -12,16 +12,16 @@ import '../App.css';
 
 const Event = props => (
   <TableRow>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{moment(props.event.start).format("MM/DD/YYYY hh:mm a")}</TableCell>
-    <TableCell>
+    <TableCell align="center" className={props.event.completed ? 'completed' : ''}>{moment(props.event.start).format("MM/DD/YYYY hh:mm a")}</TableCell>
+    <TableCell align="center">
         <Link to={"/complete/"+props.event._id}>Complete</Link>
     </TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.title}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.phone}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.email_address}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}><a href={"https://www.google.com/maps/search/?api=1&query="+props.event.job_address}>{props.event.job_address}</a></TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{moment(props.event.end).format("MM/DD/YYYY hh:mm a")}</TableCell>
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.work_requested}</TableCell>
+    <TableCell align="center" className={props.event.completed ? 'completed' : ''}>{props.event.title}</TableCell>
+    <TableCell align="center" className={props.event.completed ? 'completed' : ''}>{props.event.phone}</TableCell>
+    <TableCell align="center" className={props.event.completed ? 'completed' : ''}>{props.event.email_address}</TableCell>
+    <TableCell align="center" className={props.event.completed ? 'completed' : ''}><a href={"https://www.google.com/maps/search/?api=1&query="+props.event.job_address}>{props.event.job_address}</a></TableCell>
+    <TableCell align="center" className={props.event.completed ? 'completed' : ''}>{moment(props.event.end).format("MM/DD/YYYY hh:mm a")}</TableCell>
+    <TableCell align="center" className={props.event.completed ? 'completed' : ''}>{props.event.work_requested}</TableCell>
     {/* <TableCell className={props.event.completed ? 'completed' : ''}>{moment(props.event.date_repaired).format("MM/DD/YYYY")}</TableCell>
     <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.performed_by}</TableCell>
     <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.repairs_performed}</TableCell>
@@ -29,8 +29,8 @@ const Event = props => (
     <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.hours}</TableCell>
     <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.materials}</TableCell>
     <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.room}</TableCell> */}
-    <TableCell className={props.event.completed ? 'completed' : ''}>{props.event.completed ? 'Complete' : 'Incomplete'}</TableCell>
-    <TableCell>
+    <TableCell align="center" className={props.event.completed ? 'completed' : ''}>{props.event.completed ? 'Complete' : 'Incomplete'}</TableCell>
+    <TableCell align="center">
         <Link to={"/edit/"+props.event._id}>Edit</Link>
     </TableCell>
   </TableRow>
@@ -85,14 +85,14 @@ export default class EventsList extends Component {
                 <Table className="table-responsive table-bordered" style={{ marginTop: 20}}>
                   <TableHead>
                     <TableRow>
-                      <TableCell>Date / Start Time</TableCell>
-                      <TableCell>Complete</TableCell>
-                      <TableCell>Client Name</TableCell>
-                      <TableCell>Phone</TableCell>
-                      <TableCell>Client Email Address</TableCell>
-                      <TableCell>Job Address</TableCell>
-                      <TableCell>End Time</TableCell>
-                      <TableCell>Work Requested</TableCell>
+                      <TableCell align="center">Date / Start Time</TableCell>
+                      <TableCell align="center">Complete</TableCell>
+                      <TableCell align="center">Client Name</TableCell>
+                      <TableCell align="center">Phone</TableCell>
+                      <TableCell align="center">Client Email Address</TableCell>
+                      <TableCell align="center">Job Address</TableCell>
+                      <TableCell align="center">End Time</TableCell>
+                      <TableCell align="center">Work Requested</TableCell>
                       {/* <TableCell>Date Repaired</TableCell>
                       <TableCell>Performed By</TableCell>
                       <TableCell>Repairs Performed</TableCell>
