f81feef15b9a195f802d742f682555768cbb73db
Matthew Tan <matthewvtan@gmail.com>
2019-07-09 19:50:06 -0500

    changed buttons back to primary from blue

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -9,38 +9,38 @@ export default class Home extends Component {
         return (
             <div className="button-container">
                 <div>
-                    <Button variant="contained" color="blue">
+                    <Button variant="contained" color="primary">
                         <Link to="/list" className="nav-link">List</Link>
                     </Button>
                 </div>
-                {/* <a href="#" class="btn-gradient blue block">
+                {/* <a href="#" class="btn-gradient primary block">
                     <Link to="/list" className="nav-link">List</Link>
                 </a> */}
                 <br /><br />
                 <div>
-                    <Button variant="contained" color="blue">
+                    <Button variant="contained" color="primary">
                         <Link to="/create" className="nav-link">New Work Order</Link>
                     </Button>
                 </div>
-                {/* <a href="#" class="btn-gradient blue block">
+                {/* <a href="#" class="btn-gradient primary block">
                     <Link to="/create" className="nav-link">New Order</Link>
                 </a> */}
                 <br /><br />
                 <div>
-                    <Button variant="contained" color="blue">
+                    <Button variant="contained" color="primary">
                         <Link to="/calendar" className="nav-link">Calendar</Link>
                     </Button>
                 </div>
-                {/* <a href="#" class="btn-gradient blue block">
+                {/* <a href="#" class="btn-gradient primary block">
                     <Link to="/calendar" className="nav-link">Calendar</Link>
                 </a> */}
                 <br /><br />
                 <div>
-                <Button variant="contained" color="blue">
+                <Button variant="contained" color="primary">
                     <Link to="/export" className="nav-link">Export</Link>
                 </Button>
                 </div>
-                {/* <a href="#" class="btn-gradient blue block">
+                {/* <a href="#" class="btn-gradient primary block">
                     <Link to="/export" className="nav-link">Export</Link>
                 </a> */}
             </div>
