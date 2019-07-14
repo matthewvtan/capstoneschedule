198453a2cd256cfa3a50566987bd3d39edc9203b
Matthew Tan <matthewvtan@gmail.com>
2019-07-14 17:19:50 -0500

    Change link to link with curly braces

--- client/src/components/calendar-view.component.js
+++ client/src/components/calendar-view.component.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "react-router-dom";
+import { Link } from 'react-router-dom';
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
