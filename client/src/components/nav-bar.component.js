a52c49101cbeaab6f428d3d6b41f02ff0281f5e9
Matthew Tan <matthewvtan@gmail.com>
2019-08-28 13:17:42 -0500

    removed duplicate menuicon import from navbar comp

--- client/src/components/nav-bar.component.js
+++ client/src/components/nav-bar.component.js
@@ -7,8 +7,6 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
-import MenuIcon from '@material-ui/icons/Menu';
-
 
 const useStyles = makeStyles(theme => ({
   root: {
