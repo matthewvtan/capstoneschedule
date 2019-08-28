0c9095854b50c81cc154556efa10eef042a783ce
Matthew Tan <matthewvtan@gmail.com>
2019-08-28 13:14:32 -0500

    added toolbar from material ui to navbar

--- client/src/components/nav-bar.component.js
+++ client/src/components/nav-bar.component.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
- 
 
-function NavBar() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+NavBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   function handleClick(event) {
@@ -17,9 +31,15 @@ function NavBar() {
     setAnchorEl(null);
   }
 
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Button
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+            <Button
         aria-owns={anchorEl ? 'simple-menu' : undefined}
         aria-haspopup="true"
         onClick={handleClick}
@@ -48,11 +68,78 @@ function NavBar() {
           </Link>
         </MenuItem>
       </Menu>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
 
-export default NavBar;
+
+
+// - - - - - - - - - - -
+
+
+// import React from 'react';
+// import Button from '@material-ui/core/Button';
+// import Menu from '@material-ui/core/Menu';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import { Link } from 'react-router-dom';
+
+ 
+
+// function NavBar() {
+//   const [anchorEl, setAnchorEl] = React.useState(null);
+
+//   function handleClick(event) {
+//     setAnchorEl(event.currentTarget);
+//   }
+
+//   function handleClose() {
+//     setAnchorEl(null);
+//   }
+
+//   return (
+//     <div>
+//       <Button
+//         aria-owns={anchorEl ? 'simple-menu' : undefined}
+//         aria-haspopup="true"
+//         onClick={handleClick}
+//       >
+//         <MenuIcon />
+//       </Button>
+//       <Menu id="simple-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+//         <MenuItem onClick={handleClose}>
+//           <Link to="/list">
+//             List
+//             </Link>
+//         </MenuItem>
+//         <MenuItem onClick={handleClose}>
+//           <Link to="/create">
+//             Create Work Order
+//           </Link>
+//         </MenuItem>
+//         <MenuItem onClick={handleClose}>
+//           <Link to="/calendar">
+//             Calendar
+//           </Link>
+//         </MenuItem>
+//         <MenuItem onClick={handleClose}>
+//           <Link to="/export">
+//             Export
+//           </Link>
+//         </MenuItem>
+//       </Menu>
+//     </div>
+//   );
+// }
+
+// export default NavBar;
 
 
 
