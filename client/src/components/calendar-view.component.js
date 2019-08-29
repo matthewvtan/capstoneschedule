ecab297cd3a3c10ad42855cde9a7654fd65a51de
Matthew Tan <matthewvtan@gmail.com>
2019-08-28 19:39:35 -0500

    testing filter function

--- client/src/components/calendar-view.component.js
+++ client/src/components/calendar-view.component.js
@@ -86,7 +86,7 @@ export default class CalendarView extends React.Component {
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
             ref={this.calendarComponentRef}
             weekends="true"
-            events={this.state.events}
+            events={filterNames(this.state.events)}
             eventClick={this.handleEventClick}
             nowIndicator="true"
             height="parent"
